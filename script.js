cca94cf400110f2d63eeaed8fb534266b8600ce6
Nitin Tembhare <nitintembhare340@gmail.com>
2023-05-07 16:06:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//redirecting to login page
+document.querySelector("#login-btn").addEventListener("click", ()=>{
+    window.location.href="./login/index.html"
+})
+//redirecting to signup page
+document.querySelector("#signup-btn").addEventListener("click", ()=>{
+    window.location.href="./signup/index.html"
+})
+
+
+
+// myProducts.filter((item)=>item.title.includes(search.value))
+
+// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
